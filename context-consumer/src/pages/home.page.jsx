ca70bd8deae0e9840283c6074f97a529e82cfaed
Francisco Gonçalves <franciscorg.dev@gmail.com>
2023-02-28 21:55:36 +0000

    updated context consumer + added related work

--- context-consumer/src/pages/home.page.jsx
+++ context-consumer/src/pages/home.page.jsx
@@ -1,6 +1,8 @@
+import React, { useEffect } from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 import { Button } from "antd";
+import axios from "axios";
 
 const PageWrapper = styled.div`
   width: 100%;
@@ -18,7 +20,27 @@ const ActionsWrapper = styled.div``;
 const StyledButton = styled(Button)`
   font-weight: bold;
 `;
+
+const ADDRESS = "http://localhost:1026";
+const GET_ENTITIES = "/v2/entities";
+
 const Home = () => {
+  useEffect(() => {
+    axios
+      .get(`${ADDRESS}${GET_ENTITIES}`, {
+        mode: "no-cors",
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Credentials": true,
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
+        },
+      })
+      .then((res) => {
+        const entities = res.data;
+        console.log(entities);
+      });
+  }, []);
   return (
     <PageWrapper>
       <TitleWrapper>
