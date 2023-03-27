37b1754e4e9cf77f81de47afd74977834f6942df
Francisco Gonçalves <franciscorg.dev@gmail.com>
2023-03-27 22:26:34 +0100

    first steps into cygnus-mysql API

--- helpers/ngsijs/index.js
+++ helpers/ngsijs/index.js
@@ -1,7 +1,7 @@
 const NGSI = require("ngsijs");
-const connection = new NGSI.Connection("http://localhost:1026");
-
+const ngsiConnection = new NGSI.Connection("http://localhost:1026");
 const express = require("express");
+const mysql = require("mysql");
 const app = express();
 const PORT = 8081;
 
@@ -12,9 +12,13 @@ app.listen(PORT, () => {
   console.log(`ngsiJS server running at: http://localhost:${PORT}/`);
 });
 
+/**
+ * ORION CONTEXT BROKER ENTITIES
+ */
+
 // Lists all entities registered in the context broker server
 app.get("/entity/list", (req, res) => {
-  connection.v2.listEntities().then((response) => {
+  ngsiConnection.v2.listEntities().then((response) => {
     res.send(response.results);
   });
 });
@@ -22,7 +26,7 @@ app.get("/entity/list", (req, res) => {
 // Creates a new entity
 app.post("/entity/create", (req, res) => {
   const entity = req.body;
-  connection.v2.createEntity(entity).then(
+  ngsiConnection.v2.createEntity(entity).then(
     (response) => {
       res.send(response);
     },
@@ -35,7 +39,7 @@ app.post("/entity/create", (req, res) => {
 // Updates attributes from an existing entity
 app.post("/entity/update", (req, res) => {
   const changes = req.body;
-  connection.v2.updateEntityAttributes(changes).then(
+  ngsiConnection.v2.updateEntityAttributes(changes).then(
     (response) => {
       res.send(response);
     },
@@ -48,7 +52,7 @@ app.post("/entity/update", (req, res) => {
 // Removes an entity from the context broker server given its id
 app.delete("/entity/:id/delete", (req, res) => {
   const entityId = req.params.id;
-  connection.v2.deleteEntity(entityId).then(
+  ngsiConnection.v2.deleteEntity(entityId).then(
     (response) => {
       res.send(response);
     },
@@ -58,10 +62,14 @@ app.delete("/entity/:id/delete", (req, res) => {
   );
 });
 
+/**
+ * ORION CONTEXT BROKER SUBSCRIPTIONS
+ */
+
 // Creates a new subscription to the context broker server
 app.post("/subscription/create", (req, res) => {
   const subscription = req.body;
-  connection.v2.createSubscription(subscription).then(
+  ngsiConnection.v2.createSubscription(subscription).then(
     (response) => {
       res.send(response);
     },
@@ -74,7 +82,7 @@ app.post("/subscription/create", (req, res) => {
 // Removes a subscription from the context broker server given its id
 app.delete("/subscription/:id/delete", (req, res) => {
   const subscriptionId = req.params.id;
-  connection.v2.deleteSubscription(subscriptionId).then(
+  ngsiConnection.v2.deleteSubscription(subscriptionId).then(
     (response) => {
       res.send(response);
     },
@@ -87,7 +95,7 @@ app.delete("/subscription/:id/delete", (req, res) => {
 // Updates an existing subscription
 app.post("/subscription/update", (req, res) => {
   const changes = req.body;
-  connection.v2.updateSubscription(changes).then(
+  ngsiConnection.v2.updateSubscription(changes).then(
     (response) => {
       res.send(response);
     },
@@ -99,7 +107,52 @@ app.post("/subscription/update", (req, res) => {
 
 // Lists all entities registered in the context broker server
 app.get("/subscription/list", (req, res) => {
-  connection.v2.listSubscriptions().then((response) => {
+  ngsiConnection.v2.listSubscriptions().then((response) => {
     res.send(response.results);
   });
+});
+
+/**
+ * CYGNUS MYSQL SINK
+ */
+
+// Create a mysql connection to cygnus-mysql sink
+var mysqlConnection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "123",
+  database: "default",
+});
+
+// Given a string, an occurence and a replacement, returns the original string with all the ocurrences replaced.
+const replaceAll = (string, occurence, replacement) =>
+  string.split(occurence).join(replacement);
+
+const getEntityType = (entityId) => {
+  const type = entityId.split(":")[0];
+  return type.charAt(0).toUpperCase() + type.slice(1);
+};
+
+const matchMySQLTableName = (id) =>
+  replaceAll(id, ":", "_") + "_" + getEntityType(id);
+
+// Lists all entries registered by Cygnus for the entity given its NGSI id
+app.get("/history/entity/:id", (req, res) => {
+  // Pre-process entity id to match mysql sink table name convention
+  const entityId = matchMySQLTableName(req.params.id);
+  const attrName = req.query.attrName;
+
+  if (attrName)
+    mysqlConnection.query(
+      `SELECT * FROM ${entityId} WHERE attrName = '${attrName}'`,
+      (err, results) => {
+        if (err) res.send(err);
+        res.send(results);
+      }
+    );
+  else
+    mysqlConnection.query(`SELECT * FROM ${entityId}`, (err, results) => {
+      if (err) res.send(err);
+      res.send(results);
+    });
 });
