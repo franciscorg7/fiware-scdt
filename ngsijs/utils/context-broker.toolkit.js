7bc8c17903bbaf8cb6fe0ee22eaa21d6fb0e1b94
Francisco Gonçalves <franciscorg.dev@gmail.com>
2023-04-14 17:04:57 +0100

    added a builder to context-broker-toolkit for cygnus subscriptions

--- ngsijs/utils/context-broker.toolkit.js
+++ ngsijs/utils/context-broker.toolkit.js
@@ -1,3 +1,5 @@
+const { CYGNUS_HOST, CYGNUS_PORT } = require("../config.js");
+
 /**
  *  Receives an NGSIv2 entity and returns the pretended entity object structure.
  *
@@ -23,4 +25,38 @@ const buildEntityDummy = (entity) => {
   return build;
 };
 
-module.exports = { buildEntity, buildEntityDummy };
+/**
+ *  Receives an NGSIv2 entity and propagates its information to a Cygnus subscription.
+ *
+ * @param {Object} entity
+ * @returns {Object} subscription
+ */
+const buildCygnusSubscription = async (entity, description) => {
+  const entityAttrs = Object.keys(entity);
+  const build = {
+    description: description,
+    subject: {
+      entities: [
+        {
+          id: entity.id,
+          type: entity.type,
+        },
+      ],
+      condition: {
+        attrs: [],
+      },
+    },
+    notification: {
+      http: {
+        url: `${CYGNUS_HOST}:${CYGNUS_PORT}/notify`, // check if these configuration params match the ones you're using for your `cygnus` docker container
+      },
+      attrs: entityAttrs.filter(
+        (attr) => attr != "id" && attr != "type" && attr != "subscriptions" // notify Cygnus only about simulation attributes
+      ),
+    },
+    expires: new Date(new Date().getFullYear() + 50, 0, 1), // make the subscription expire in 50 years
+  };
+  return build;
+};
+
+module.exports = { buildEntity, buildEntityDummy, buildCygnusSubscription };
