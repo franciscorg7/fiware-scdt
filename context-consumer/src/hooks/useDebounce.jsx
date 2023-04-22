159d4074f82621aeaa73d4c528913ebe5d7fcf73
Francisco Gonçalves <franciscorg.dev@gmail.com>
2023-04-23 00:17:24 +0100

    ✨ added custom hook for event debouncing

---
+++ context-consumer/src/hooks/useDebounce.jsx
@@ -0,0 +1,22 @@
+import { useEffect } from "react";
+
+/**
+ * Custom hook that sets a timer to delay the execution of the
+ * callback, and resetting the timer each time a new event is
+ * received. This is called debouncing and allows the callback
+ * to only be executed after a certain amount of time has passed
+ * without new events being received.
+ *
+ * @param {Function} callback
+ * @param {Number} delay
+ * @param {Array} dependencies
+ */
+export function useDebounce(callback, delay, dependencies) {
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      callback();
+    }, delay);
+
+    return () => clearTimeout(timeout);
+  }, dependencies);
+}
