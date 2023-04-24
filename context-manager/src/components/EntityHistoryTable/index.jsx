d88464ec82df1276e04bd0f6ab98aa05c201b610
Francisco Gonçalves <franciscorg.dev@gmail.com>
2023-04-24 16:04:52 +0100

    📦 added momentjs + parsed recvTime on entity history table

--- context-manager/src/components/EntityHistoryTable/index.jsx
+++ context-manager/src/components/EntityHistoryTable/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Table, Tag } from "antd";
 import typeTagService from "../../services/type-tag";
+import moment from "moment";
 
 const EntityHistoryTable = ({ history }) => {
   // History columns definition
@@ -29,6 +30,10 @@ const EntityHistoryTable = ({ history }) => {
       title: "Date/Time",
       dataIndex: "recvTime",
       key: "recvTime",
+      render: (recvTime) => {
+        const parsedRecvTime = moment(recvTime, "YYYY-MM-DD HH:mm:ss.SSS");
+        return parsedRecvTime.format("DD MMM YYYY (HH:mm:ss.SSS)");
+      },
     },
   ];
   // Apply a key value to each history entry (let React identify table rows)
