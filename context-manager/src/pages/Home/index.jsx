8082d20b5df7587ab3555560d8de43580de71671
Francisco Gonçalves <franciscorg.dev@gmail.com>
2023-04-25 02:12:24 +0100

    🚧 working on the new home page

--- context-manager/src/pages/Home/index.jsx
+++ context-manager/src/pages/Home/index.jsx
@@ -1,6 +1,30 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const BodyWrapper = styled.div`
+  padding: 42px;
+  height: 100%;
+  flex: 1;
+`;
+const NavigationButton = styled.div``;
 const HomePage = () => {
-  return <div>home</div>;
+  return (
+    <BodyWrapper>
+      <Link to="/entity/list">
+        <NavigationButton>Entities</NavigationButton>
+      </Link>
+      <Link to="/entity/list">
+        <NavigationButton></NavigationButton>
+      </Link>
+      <Link to="/entity/list">
+        <NavigationButton></NavigationButton>
+      </Link>
+      <Link to="/entity/list">
+        <NavigationButton></NavigationButton>
+      </Link>
+    </BodyWrapper>
+  );
 };
 
 export default HomePage;
