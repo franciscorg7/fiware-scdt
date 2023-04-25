656f3886c0f245c84d4fbfb67fcdd10916f8c440
Francisco Gonçalves <franciscorg.dev@gmail.com>
2023-04-25 01:22:03 +0100

    🔗 updated router

--- context-manager/src/Main.js
+++ context-manager/src/Main.js
@@ -1,13 +1,16 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import HomePage from "./pages/Home";
 import EntityPage from "./pages/Entity";
+import EntityListPage from "./pages/EntityList";
 
 const Main = () => {
   return (
     <Routes>
       <Route exact path="/" element={<HomePage />} />
+      <Route exact path="/entity/list" element={<EntityListPage />} />
       <Route path="/entity/:id" element={<EntityPage />} />
+      <Route path="*" element={<Navigate to="/" />} />
     </Routes>
   );
 };
