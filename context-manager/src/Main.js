8c50a7dd4cdc2cb968a3efd17b581ca265b10854
Francisco Gonçalves <franciscorg.dev@gmail.com>
2023-04-25 18:50:06 +0100

    🔗 updated routes

--- context-manager/src/Main.js
+++ context-manager/src/Main.js
@@ -3,12 +3,22 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import HomePage from "./pages/Home";
 import EntityPage from "./pages/Entity";
 import EntityListPage from "./pages/EntityList";
+import RepetitionListPage from "./pages/RepetitionList";
+import ComparePage from "./pages/Compare";
+import SubscriptionListPage from "./pages/SubscriptionList";
 
 const Main = () => {
   return (
     <Routes>
       <Route exact path="/" element={<HomePage />} />
       <Route exact path="/entity/list" element={<EntityListPage />} />
+      <Route exact path="/repetition/list" element={<RepetitionListPage />} />
+      <Route exact path="/compare" element={<ComparePage />} />
+      <Route
+        exact
+        path="/subscription/list"
+        element={<SubscriptionListPage />}
+      />
       <Route path="/entity/:id" element={<EntityPage />} />
       <Route path="*" element={<Navigate to="/" />} />
     </Routes>
