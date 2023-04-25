a0d097742e11f01923a98da361b877f17db18c06
Francisco Gonçalves <franciscorg.dev@gmail.com>
2023-04-25 01:22:27 +0100

    ✨ added search bar feature

--- context-manager/src/components/Navbar/index.jsx
+++ context-manager/src/components/Navbar/index.jsx
@@ -1,23 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import { highlightOrange, textBlue } from "../../palette";
 import styled from "styled-components";
 import { SearchOutlined } from "@ant-design/icons";
 import { Input, Row, Col } from "antd";
+import { Link, useNavigate } from "react-router-dom";
+import { useDebounce } from "../../hooks/useDebounce";
 
 const NavbarWrapper = styled(Row)`
   justify-content: flex-start;
   align-items: center;
   padding: 0 24px;
   background: #fff;
-  color: ${highlightOrange};
-  & span {
-    color: ${textBlue};
-  }
+  z-index: 10;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1), 0 2px 10px rgba(0, 0, 0, 0.1);
 `;
 
 const TitleWrapper = styled(Col)`
+  height: fit-content;
   justify-content: flex-start;
   font-size: 12px;
+  user-select: none;
+  & span {
+    color: ${textBlue};
+  }
+  color: ${highlightOrange};
 `;
 const SearchBarWrapper = styled(Col)`
   position: absolute;
@@ -29,15 +35,39 @@ const SearchBarWrapper = styled(Col)`
 const SearchBar = styled(Input)``;
 
 const Navbar = () => {
+  const [searchValue, setSearchValue] = useState("");
+
+  // Use 1000ms debouncing to handle onEntitySearch
+  useDebounce(() => onEntitySearch(searchValue), 1000, [searchValue]);
+
+  // Initialize useNavigate hook
+  const navigate = useNavigate();
+
+  /**
+   * Navigate to /entity/list passing the current search value
+   * via the location state.
+   *
+   * @param {string} searchValue
+   */
+  const onEntitySearch = (searchValue) => {
+    navigate("/entity/list", { state: { idPattern: searchValue } });
+  };
+
   return (
     <NavbarWrapper>
-      <TitleWrapper>
-        <h1>
-          <span>FIWARE</span> Context Manager
-        </h1>
-      </TitleWrapper>
+      <Link to="/">
+        <TitleWrapper>
+          <h1>
+            <span>FIWARE</span> Context Manager
+          </h1>
+        </TitleWrapper>
+      </Link>
       <SearchBarWrapper>
-        <SearchBar size="medium" prefix={<SearchOutlined />} />
+        <SearchBar
+          size="medium"
+          prefix={<SearchOutlined />}
+          onChange={(e) => setSearchValue(e.target.value)}
+        />
       </SearchBarWrapper>
     </NavbarWrapper>
   );
