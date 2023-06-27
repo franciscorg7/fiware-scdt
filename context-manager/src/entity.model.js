9f9c0eb54a8b129b901a00350f668de4484da7ed
Francisco Gonçalves <franciscorg.dev@gmail.com>
2023-06-27 21:57:10 +0100

    🚚 removed unused js files

--- context-manager/src/entity.model.js
+++
@@ -1,8 +0,0 @@
-const Vehicle = {
-  id, // id in NGSIv2 format (type:Name:index)
-  type, // string
-  from, // edge id
-  to, // edge id
-  depart, // start time step
-  blueprint, // vehicle's blueprint id
-};
